f41b7cc76efd37786f4b643528e1c8e81f6e95e9
JoseRicardoHolguin <itit@iMac-de-ULSA-7.local>
2026-05-05 14:46:09 -0600

    proyecto inicial

---
+++ PatientsSystem/PatientsSystem/PatientsSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatientsSystemApp.swift
+//  PatientsSystem
+//
+//  Created by ITIT on 05/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PatientsSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
